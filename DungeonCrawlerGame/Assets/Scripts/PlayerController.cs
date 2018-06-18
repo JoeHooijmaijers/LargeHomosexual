9e98e19f02d4c31acecd9810342cae86dd8db0ca
JoeHooijmaijers <JoeHooijmaijers@users.noreply.github.com>
2018-06-18 14:20:38 +0200

    Init Commit
    Dungeon Crawler ecksde

---
+++ DungeonCrawlerGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed = 1.0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Horizontal player movement
+		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < 0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime, 0f));
+        }
+        //Vertical player movement
+        if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") > 0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime));
+        }
+
+        //Basic Attack
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+        }
+	}
+}
---
+++ DungeonCrawlerGame/Assets/Scripts/PublicEnums.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublicEnums : ScriptableObject {
+
+    enum GameClass
+    {
+        Warrior = 1,
+        Archer,
+        Mage,
+        Assasin,
+        Gunslinger,
+        Enchanter
+    }
+
+    enum WeaponType
+    {
+        OneHandedSword =1,
+        TwoHandedSword,
+        Lance,
+        Dagger,
+        Longbow,
+        Crossbow,
+        Wand,
+        Staff,
+        Orb
+
+    }
+
+    enum ConsumableType
+    {
+        
+    }
+    
+}
+
